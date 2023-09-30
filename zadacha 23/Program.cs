c0a7746600da62e92078328950f2e87af08b4ad7
Elena <knif-fly@mail.ru>
2023-09-30 18:35:01 +0300

    Добавила задачу 23

---
+++ zadacha 23/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число(N)
+// и выдает таблицу кубов чисел от 1 до N.
+// 3 -> 1,4,9
+// 5 -> 1,8,27,64,125
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("ShowCube(N)");
+for (int i=1; i<=n; i++)
+{
+int k = i*i*i;
+Console.Write($"{k}, ");
+}
